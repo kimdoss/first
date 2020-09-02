fac1813290893abd9b6705b1c0e3496db855ddb4
Sik <kjskjs87@gmail.com>
2020-09-02 16:52:25 +0900

    no message

---
+++ BuildTest/Assets/Editor/BuildEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BuildEditor : MonoBehaviour
+{
+    public static bool BuildPlayer()
+    {
+        Debug.Log("Build Player");
+
+        UnityEditor.BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.target = BuildTarget.Android;
+
+        var buildReport = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        var buildSummary = buildReport.summary;
+
+        return buildSummary.result == UnityEditor.Build.Reporting.BuildResult.Succeeded;
+    }
+}
